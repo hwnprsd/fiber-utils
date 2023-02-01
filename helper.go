d2759922236330086b7718c48c12f47fdbca6beb
Ashwin Prasad <ashwin@onpar.in>
2023-02-01 12:48:38 +0530

    added helper

--- helper.go
+++ helper.go
@@ -1,3 +1,11 @@
 package fiberutils
 
 type Map map[string]interface{}
+
+func NewRequestError(statusCode int, message string, err error) *RequestError {
+	return &RequestError{
+		StatusCode: statusCode,
+		Message:    message,
+		Err:        err,
+	}
+}
